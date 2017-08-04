7a5c697f16e92096da21e01c0cb573bdaa209c48
Niirok <anthony.youssef@telecomnancy.net>
2017-08-04 12:41:16 +0200

    added something to test bis

--- BoostUnitTest2/BoostUnitTest.cpp
+++ BoostUnitTest2/BoostUnitTest.cpp
@@ -2,8 +2,6 @@
 #include "stdafx.h"
 #include "myComplex.cpp"
 
-using namespace std;
-
 #define BOOST_TEST_MODULE Boost.Test myComplex tests;
 
 myComplex a(3, 5);
@@ -15,15 +13,10 @@ BOOST_AUTO_TEST_CASE( BoostUnitTest )
     myComplex b(3, 0);
 
     BOOST_CHECK(a.getReal() == 3);
-    cout << "a.real = 3 ok" << endl;
-
 
     BOOST_CHECK(a.getImag() == 5);
-    cout << "a.img = 5 ok" << endl;
-
 
     BOOST_CHECK((a.getReal()+b.getReal()) == 6);
-    cout << "real(a)+real(b) = 6 ok" << endl;
 
 }
 
